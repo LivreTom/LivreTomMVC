1fb7a942e987de41bd98c2a43d8c6ceccec1ce26
WaldsonLima <waldsonh@gmail.com>
2026-03-11 22:27:45 -0300

    Refat UseHttpsDirection

--- LivreTom/Program.cs
+++ LivreTom/Program.cs
@@ -86,7 +86,11 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+if (app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
+
 app.UseStaticFiles();
 
 app.UseRouting();
